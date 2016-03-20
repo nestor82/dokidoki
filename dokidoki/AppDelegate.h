7b33d2e273563e6e9e4af1a02099c029cb94559f
Eugene <reverseddt@naver.com>
2016-03-20 17:59:32 +0900

    Init project

---
+++ dokidoki/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dokidoki
+//
+//  Created by Eugene on 2016. 3. 20..
+//  Copyright © 2016년 Eugene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dokidoki/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dokidoki
+//
+//  Created by Eugene on 2016. 3. 20..
+//  Copyright © 2016년 Eugene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
